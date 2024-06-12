6a108fcdcfb8c0bd239341e9ea8fca0b69e73eec
dmipor333 <dmipor333@ya.ru>
2024-06-12 17:13:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+a = 'жираф упал'
+print(len(a))   # длина строки
+
+first = int(10)
+second = int(5)
+summa = first + second # вычисляю сумму
+deff = first - second  # вычисляю разность
+print(summa , deff)
+
+first = 8
+second = 9
+third = 10
+mean = ((first + second + third)/3) # вычисляю среднеарифметическую
+print(mean)
+
+first_string = 'Вторник'
+second_string = 'Понедельник,'
+print(second_string,first_string)
+
+a = 5
+b = 6
+c = 7
+f = (a*b)+(a*c)
+result = (f**3)/2            # вычисляю результат
+print(result)
+
+
+
+
